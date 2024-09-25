3a0ca202f26dca47e04fd9340a507d951f00d742
Rimpa Adhikary <141876300+Rimpa-Adhikary@users.noreply.github.com>
2024-09-26 01:56:47 +0530

    Delete hw5.cpp

--- hw5.cpp
+++
@@ -1,9 +0,0 @@
-//To check whether a number divisible by 5 or 8
-
-#include<stdio.h>
-int main()
-{
-	int number = 25;
-	((number % 5 == 0) || (number % 8 == 0)) ? printf ("Yes") : printf ("no");
-	return 0;
-}
