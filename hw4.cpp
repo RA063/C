0f0dda38a01fab97285bccd4d3bf7be8324f8bea
Rimpa Adhikary <141876300+Rimpa-Adhikary@users.noreply.github.com>
2024-09-26 01:57:07 +0530

    Delete hw4.cpp

--- hw4.cpp
+++
@@ -1,9 +0,0 @@
-//To find a number +ve,-ve or 0
-
-#include<stdio.h>
-int main()
-{
-int number = 21;
-(number > 0) ? printf("%d is +ve",number) : (number < 0) ? printf ("%d is -ve",number) : printf ("0");
-return 0;
-}
